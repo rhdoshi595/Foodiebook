6119bb02071c35b121e7fee0d70dc27f0b282b4c
rhdoshi595 <rhdoshi595@gmail.com>
2017-01-12 11:53:32 -0500

    Removed minification warning

---
+++ webpack.config.prod.js
@@ -0,0 +1,40 @@
+var webpack = require("webpack");
+
+const path = require("path");
+
+module.exports = {
+  context: __dirname,
+  entry: "./frontend/facebook.jsx",
+  output: {
+    path: path.join(__dirname, 'app', 'assets', 'javascripts'),
+    filename: "bundle.js"
+  },
+  plugins:[
+    new webpack.DefinePlugin({
+      'process.env':{
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress:{
+        warnings: true
+      }
+    })
+  ],
+  module: {
+    loaders: [
+      {
+        test: [/\.jsx?$/, /\.js?$/],
+        exclude: /(node_modules|browser_components)/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  },
+  devtool: 'source-maps',
+  resolve: {
+    extensions: ["", ".js", ".jsx"]
+  }
+};
