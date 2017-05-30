2914db48d96c0413f32c3d20967e80be5c103222
rhdoshi595 <rhdoshi595@gmail.com>
2017-05-30 13:55:57 -0400

    Deleted testing of editing post action on entry file

--- frontend/facebook.jsx
+++ frontend/facebook.jsx
@@ -2,10 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from './components/root';
-import { editPost } from './actions/post_actions';
-
-window.editPost = editPost;
-
 
 document.addEventListener("DOMContentLoaded", () => {
   let store;
